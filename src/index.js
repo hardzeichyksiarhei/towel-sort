9410cb0be2a42853e8a448bb12127b25595d1b15
hardz <hardzeichyksiarhei@gmail.com>
2020-09-09 19:49:45 +0300

    feat: "towelSort" feature implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    return matrix.reduce((acc, curr, idx) => {
+        if (idx % 2 === 0) return acc.concat(curr);
+        return acc.concat(curr.reverse());
+    }, []);
+};
